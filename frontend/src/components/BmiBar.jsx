e7eabea8f06ea080a36fb4f8d09f065f18175e57
Xomil <xomilol@gmail.com>
2025-11-10 14:55:59 +0100

    add bmiBar animation

--- frontend/src/components/BmiBar.jsx
+++ frontend/src/components/BmiBar.jsx
@@ -1,29 +1,17 @@
-// import React from "react";
-//
-// export default function BMIBar({ value }) {
-//     const minBMI = 10;
-//     const maxBMI = 40;
-//     const position = Math.min(Math.max(((value - minBMI) / (maxBMI - minBMI)) * 100, 0), 100);
-//
-//     return (
-//         <div className="bmiBar">
-//             <div className="bmiSection" style={{ backgroundColor: "orange", flex: 18.5 - 10, borderTopLeftRadius: "16px", borderBottomLeftRadius: "16px" }}></div>
-//             <div className="bmiSection" style={{ backgroundColor: "green", flex: 25 - 18.5 }}></div>
-//             <div className="bmiSection" style={{ backgroundColor: "yellow", flex: 30 - 25 }}></div>
-//             <div className="bmiSection" style={{ backgroundColor: "red", flex: 40 - 30, borderTopRightRadius: "16px", borderBottomRightRadius: "16px" }}></div>
-//
-//             <div className="bmiMarker" style={{ left: `50%` }}></div>
-//         </div>
-//     );
-// }
-
-import React from "react";
+import React, {useEffect, useRef, useState} from "react";
+import {roundDec} from "../api/utils.js";
 
 export default function BMIBar({ value }) {
+    const [position, setPosition] = useState(50)
+
     const minBMI = 10;
     const maxBMI = 40;
-    const position = Math.min(Math.max(((value - minBMI) / (maxBMI - minBMI)) * 100, 0), 100);
-    console.log(position)
+    const targetPosition = roundDec(Math.min(Math.max(((value - minBMI) / (maxBMI - minBMI)) * 100, 0), 100), 1)
+
+    useEffect(() => {
+        setPosition(targetPosition);
+    }, [targetPosition]);
+
     return (
         <div className="bmiBar">
             <div
