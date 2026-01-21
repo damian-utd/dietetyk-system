c16960dbc7fa1149d0787971f95f1d1e6bc47b85
Xomil <xomilol@gmail.com>
2026-01-21 14:04:50 +0100

    add request handling on frontend

--- frontend/src/api/app/diet.js
+++ frontend/src/api/app/diet.js
@@ -1,7 +1,7 @@
 // diet.js
 
 export async function searchProducts(search) {
-    const res = await fetch ("/api/diet/search", {
+    const res = await fetch("/api/diet/search", {
         method: "POST",
         headers: {"Content-Type" : "application/json"},
         body: JSON.stringify({search}),
@@ -12,6 +12,68 @@ export async function searchProducts(search) {
 
     if (!res.ok) {
         throw new Error (data.message)
+    }
+
+    return data
+}
+
+export async function savePlan(planState) {
+    const res = await fetch("/api/diet/save", {
+        method: "POST",
+        headers: {"Content-Type" : "application/json"},
+        body: JSON.stringify({planState}),
+        credentials: "include"
+    })
+
+    const data = await res.json()
+
+    if (!res.ok) {
+        throw new Error (data.message)
+    }
+
+    return data
+}
+
+export async function getPlans() {
+    const res = await fetch("/api/diet/get", {
+        credentials: "include"
+    })
+
+    const data = await res.json()
+
+    if (!res.ok) {
+        throw new Error (data.message)
+    }
+
+    return data
+}
+
+export async function getPlansCount() {
+    const res = await fetch("/api/diet/count", {
+        credentials: "include"
+    })
+
+    const data = await res.json()
+
+    if (!res.ok) {
+        throw new Error(data.message)
+    }
+
+    return {
+        value: data.count,
+        title: "Liczba planów"
+    }
+}
+
+export async function getPlanById(id) {
+    const res = await fetch(`api/diet/get/${id}`, {
+        credentials: "include"
+    })
+
+    const data = await res.json()
+
+    if (!res.ok) {
+        throw new Error(data.message)
     }
 
     return data
