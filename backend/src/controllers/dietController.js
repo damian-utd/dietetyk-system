88fc86bec4de09bbcbd5395dbb640c43048be6cc
Xomil <xomilol@gmail.com>
2026-01-21 14:05:57 +0100

    clean up code structure

--- backend/src/controllers/dietController.js
+++ backend/src/controllers/dietController.js
@@ -2,11 +2,15 @@ import { appDb, productsDb } from "../config/db.js";
 
 export async function searchProducts(req, res) {
     try {
-        if (req.user.role !== "dietetyk") return res.status(403).json("Brak uprawnień do wykonania operacji")
+        if (req.user.role !== "dietetyk") return res.status(403).json({
+            message: "Brak uprawnień do wykonania operacji"
+        })
 
         const search = req.body.search
 
-        if(!search || search.length < 3) return res.status(400).json("Zbyt krótkie lub brak zapytania")
+        if(!search || search.length < 3) return res.status(400).json({
+            message: "Zbyt krótkie lub brak zapytania"
+        })
 
         const searchArray = search.split(" ").filter(word => word !== "").map((s, index) => {
             if (s !== "") {
@@ -14,7 +18,9 @@ export async function searchProducts(req, res) {
             }
         })
 
-        if(!searchArray) return res.status(400).json("Zbyt krótkie lub brak zapytania")
+        if(!searchArray) return res.status(400).json({
+            message: "Zbyt krótkie lub brak zapytania"
+        })
 
         const ilikes = searchArray.map((s, index) => {
             if (index === 0) {
