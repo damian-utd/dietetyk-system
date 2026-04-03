1422416b54e7ab952a640158927a69299b851eba
Xomil <xomilol@gmail.com>
2026-04-03 14:07:35 +0200

    refractor Settings component

--- frontend/src/pages/App/settings/Settings.jsx
+++ frontend/src/pages/App/settings/Settings.jsx
@@ -1,8 +1,12 @@
 import React from "react"
 import {Form, useLoaderData} from "react-router-dom";
 import {getLoggedDietician, updateLoggedDietician} from "../../../api/app/diet.js";
+import {requireAuth} from "../../../utils/utils.js";
+import styles from "./Settings.module.css"
+import patientStyles from "../patients/Patients.module.css"
 
-export async function loader() {
+export async function loader({request}) {
+    await requireAuth(request)
     return await getLoggedDietician()
 }
 
@@ -21,26 +25,38 @@ export async function action({request}) {
 export default function Settings() {
     const loaderData = useLoaderData()
 
-    console.log(loaderData)
-
     return (
-        <section>
+        <section className={styles.settingsBody}>
             <Form method="POST">
-                <input
-                    type={'text'}
-                    name={'full_name'}
-                    defaultValue={loaderData.full_name}
-                    placeholder={'Imię i nazwisko'}
-                />
-                <input
-                    type={'text'}
-                    name={'specialization'}
-                    defaultValue={loaderData.specialization}
-                    placeholder={'Specjalizacja'}
-                />
-                <button>
-                    Zapisz
-                </button>
+                <div className={styles.inputGroup}>
+                    <label htmlFor="{'full_name'}">
+                        Imię i nazwisko
+                    </label>
+                    <input
+                        type={'text'}
+                        name={'full_name'}
+                        defaultValue={loaderData.full_name}
+                        placeholder={'Imię i nazwisko'}
+                        className={styles.settingsInput}
+                    />
+                </div>
+                <div className={styles.inputGroup}>
+                    <label htmlFor="{'specialization'}">
+                        Specjalizacja
+                    </label>
+                    <input
+                        type={'text'}
+                        name={'specialization'}
+                        defaultValue={loaderData.specialization}
+                        placeholder={'Specjalizacja'}
+                        className={styles.settingsInput}
+                    />
+                </div>
+                <div>
+                    <button className={patientStyles.addPatientButton}>
+                        Zapisz
+                    </button>
+                </div>
             </Form>
         </section>
     )
