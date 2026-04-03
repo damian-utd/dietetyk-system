77833e617ef2ed27c9ba610513dda03cd49f592a
Xomil <xomilol@gmail.com>
2026-04-03 14:02:50 +0200

    add patientsPlans to patientsAnalysis

--- frontend/src/pages/App/patients/PatientsAnalysis.jsx
+++ frontend/src/pages/App/patients/PatientsAnalysis.jsx
@@ -1,18 +1,20 @@
 import React, {useState} from "react"
-import {useOutletContext} from "react-router-dom";
+import {NavLink, useOutletContext} from "react-router-dom";
 import {requireAuth, roundDec} from "../../../utils/utils.js";
 import {calcBMI, calcBMR, calcTDEE} from "../../../utils/calcs.js"
 import styles from "./Patients.module.css"
 import Cards from "../../../components/Cards.jsx";
 import PatientsProgress from "./components/PatientsProgress.jsx";
 import PatientsNotes from "./components/PatientsNotes.jsx";
+import Table from "../../../components/Table.jsx";
+import patientStyles from "./Patients.module.css";
 
 export async function loader({ request }) {
     await requireAuth(request)
 }
 
 export default function PatientsAnalysis() {
-    const patient = useOutletContext()
+    const { patient, setPatient, plans } = useOutletContext()
     const [patientsWeight, setPatientsWeight] = useState(patient.weight)
 
     const bmi = calcBMI(patientsWeight, patient.height)
@@ -26,21 +28,41 @@ export default function PatientsAnalysis() {
     }
 
     return (
-        <section className={styles.patientsSection}>
+        <section className={styles.patientsSection} style={{gap: "2rem"}}>
             <Cards
                 data={calcs}
                 className="big"
             />
 
-            <div>
+            <div className={styles.patientsOverview}>
                 <PatientsProgress
                     patient={patient}
+                    setPatient={setPatient}
                     setPatientsWeight={setPatientsWeight}
                 />
                 <PatientsNotes
                     patient={patient}
                 />
             </div>
+            <section className={styles.plansSection}>
+                {plans?.length > 0 &&
+                    <>
+                        <Table
+                            title={"Plany pacjenta"}
+                            headers={["Tytuł", "Opis", "Data utworzenia"]}
+                            data={plans}
+                            showLink={"plans"}
+                        />
+                        <div className={patientStyles.buttonContainer} style={{width: "92rem"}}>
+                            <NavLink to={`/plans/create?patientId=${patient.id}`}
+                                     className={patientStyles.addPatientButton}>
+                                Utwórz plan dla tego pacjenta
+                            </NavLink>
+                        </div>
+                    </>
+
+                }
+            </section>
         </section>
     )
 }
