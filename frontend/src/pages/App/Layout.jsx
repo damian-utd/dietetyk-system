beb7c114baca6cb233042efe6cec84e97a022f73
Xomil <xomilol@gmail.com>
2026-04-03 13:54:37 +0200

    add settings navlink to layout, swap hardcoded name of dietician to fetched data

--- frontend/src/pages/App/Layout.jsx
+++ frontend/src/pages/App/Layout.jsx
@@ -1,17 +1,21 @@
-import React from "react"
+import React, {useState} from "react"
 import {NavLink, Outlet, useLoaderData, useNavigate} from "react-router-dom";
 
 import { logout } from "../../api/auth/auth.js";
 
 import styles from "./Main.module.css"
 import {requireAuth} from "../../utils/utils.js";
+import {getLoggedDietician} from "../../api/app/diet.js";
 
 export async function loader({ request }) {
     await requireAuth(request)
+    return await getLoggedDietician()
 }
 
-export default function Layout() {
 
+
+export default function Layout() {
+    const dietician = useLoaderData()
     const navigate = useNavigate()
 
     async function handleLogout() {
@@ -31,7 +35,6 @@ export default function Layout() {
     return (
         <div className={styles.mainBody}>
             <nav className={styles.navBar}>
-
                 <NavLink
                     to="dashboard"
                     style={({isActive}) => isActive ? activeStyles : null}
@@ -66,11 +69,28 @@ export default function Layout() {
                     <i className="ri-logout-circle-line"></i>
                     Wyloguj
                 </button>
+                <a href="https://remixicon.com" className={styles.remix}>
+                    Icons by Remix Icon
+                </a>
             </nav>
             <section className={styles.container}>
                 <header>
-                    <h1>Damian Ratańczuk - dietetyk kliniczny</h1> {/* todo w loaderze pobrac zalogowanego dietetyka */}
-                    <i className="ri-user-line icon"></i>
+                        <h1>
+                            {dietician?.full_name &&
+                                <span>
+                                    {dietician.full_name}
+                                </span>
+                            }
+                            {(dietician.full_name && dietician.specialization) && <i className="ri-separator" style={{marginInline: '4px'}}></i>}
+                            {dietician?.specialization &&
+                                <span>
+                                    {dietician.specialization}
+                                </span>
+                            }
+                        </h1>
+                    <NavLink to={"settings"}>
+                        <i className="ri-user-settings-line"></i>
+                    </NavLink>
                 </header>
                 <Outlet/>
             </section>
