281b7652183efdc26be8176e14d0288b660fc1a3
Xomil <xomilol@gmail.com>
2026-04-03 14:05:46 +0200

    rebuild patientsProgress

--- frontend/src/pages/App/patients/components/PatientsProgress.jsx
+++ frontend/src/pages/App/patients/components/PatientsProgress.jsx
@@ -1,30 +1,46 @@
 import React, {useEffect, useState} from "react"
 import {createProgress, deleteProgress, getProgress} from "../../../../api/app/progress.js";
 import {roundDec} from "../../../../utils/utils.js";
+import Table from "../../../../components/Table.jsx";
+import styles from "../Patients.module.css"
 
-export default function PatientsProgress({ patient, setPatientsWeight }) {
+export default function PatientsProgress({ patient, setPatient, setPatientsWeight }) {
 
     const [progress, setProgress] = useState([])
     const [newWeight, setNewWeight] = useState(patient.weight)
 
     useEffect(  () => {
         const func = async () => await getProgress(patient.id)
-        func().then(res => setProgress(res))
+        func().then(res =>
+            setProgress(res.map(r => {
+                return {
+                    id: r.id,
+                    name: r.new_weight.concat(" kg"),
+                    createdAt: r.created_at.slice(0, 10)
+                }
+            }))
+        )
     }, [patient]);
 
     const handleCreateProgress = async (weight, id) => {
         await createProgress(weight, id).then(res => {
             setProgress(prev => {
                 return [
-                    ...prev,
                     {
                         id: res.id,
-                        new_weight: res.new_weight,
-                        created_at: res.created_at
-                    }
+                        new_weight: res.new_weight.concat(" kg"),
+                        created_at: res.created_at.slice(0, 10)
+                    },
+                    ...prev
                 ]
             })
             setPatientsWeight(roundDec(res.new_weight, 2))
+            setPatient(prev => {
+                return {
+                    ...prev,
+                    weight: roundDec(res.new_weight, 2)
+                }
+            })
         })
     }
     const handleDeleteProgress = async (id) => {
@@ -32,28 +48,32 @@ export default function PatientsProgress({ patient, setPatientsWeight }) {
         setProgress(prev => prev.filter(p => p.id !== id))
     }
 
-    const progressList = progress === null ?
-        <div></div> :
-        progress.map(p => {
-            return (
-                <div key={p.id}>
-                    <span>{roundDec(p.new_weight, 2)} kg </span>
-                    <span>{p.created_at.slice(0, 10)} </span>
-                    <button onClick={() => handleDeleteProgress(p.id)}>Usuń</button>
-                </div>
-            )
-        })
-
     return (
-        <div>
-            <input
-                value={newWeight}
-                onChange={(e) =>
+        <section className={styles.progressSection}>
+            {progress.length > 0 &&
+                <Table
+                    title={"Progres pacjenta"}
+                    headers={["Waga", "Data"]}
+                    data={progress}
+                    delFunc={handleDeleteProgress}
+                    delText="Czy na pewno chcesz usunąć progres"
+                    width="half"
+                />
+            }
+            <div className={styles.inputGroup} style={{flexDirection: "row", alignItems: "center", gap: "2rem"}}>
+                <button
+                    onClick={() => handleCreateProgress(newWeight, patient.id)}
+                    className={styles.addPatientButton}
+                >
+                    Dodaj progres
+                </button>
+                <input
+                    value={newWeight}
+                    onChange={(e) =>
                         setNewWeight(e.target.value)
-                }
-            />
-            <button onClick={() => handleCreateProgress(newWeight, patient.id)}>Dodaj progres</button>
-            {progressList}
-        </div>
+                    }
+                />
+            </div>
+        </section>
     )
 }
